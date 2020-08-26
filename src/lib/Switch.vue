8b20fce1098e98b5098fe4cbeaff1f761904ea89
Jsmond2016 <jsmond2016@gmail.com>
2020-08-26 16:06:54 +0800

    完成动画效果，去除button outline 边框

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -42,6 +42,7 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 / 2;
+  transition: left 250ms;
 }
 
 button.checked {
@@ -49,6 +50,10 @@ button.checked {
 }
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
+}
+
+button:focus {
+  outline: none;
 }
   
 </style>
