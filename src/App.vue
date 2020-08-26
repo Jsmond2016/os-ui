b51a28a08354f9d589bf203a39ea02dc059d9c8b
Jsmond2016 <jsmond2016@gmail.com>
2020-08-26 14:17:05 +0800

    封装 topnav 组件，完善 css

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,4 @@
 <template>
-  <div>
-    导航栏 | 
-    <router-link to="/">Home页</router-link> &nbsp;
-    <router-link to="/doc">文档页</router-link>
-  </div>
-  <hr>
   <router-view />
 </template>
 
