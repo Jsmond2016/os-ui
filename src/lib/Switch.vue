11d1b0681e3ddcafc161aed6313a9d279ea0b115
Jsmond2016 <jsmond2016@gmail.com>
2020-08-26 15:54:20 +0800

    新建 switch 组件，完成 hover 向右滚动

---
+++ src/lib/Switch.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+     <button>
+       <span></span>
+     </button>
+  </div>
+</template>
+
+<script lang='ts'>
+export default {
+  name: 'Switch'
+}
+</script>
+
+<style lang="scss" scoped>
+
+$h: 22px;
+$h2: $h - 4px;
+
+button {
+  width: $h*2;
+  height: $h;
+  border: none;
+  background: blue;
+  border-radius: $h / 2;
+  position: relative;
+}
+
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background: white;
+  border-radius: $h2 / 2;
+}
+
+button:hover > span {
+  left: calc(100% - #{$h2} - 2px);
+}
+  
+</style>
