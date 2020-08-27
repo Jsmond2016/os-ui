bc50b50462a4fdbba0d853cd01e05359cab5aa82
Jsmond2016 <jsmond2016@gmail.com>
2020-08-27 10:59:01 +0800

    完成 button 组件初始化

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-  Button 示例
+   <Button></Button>
   </div>
 </template>
+
+<script>
+import Button from '../lib/Button.vue'
+
+export default {
+  components: {
+    Button,
+  }
+}
+</script>
