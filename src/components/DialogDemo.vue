023cdb850ea20ee5022cf31d1d8b0c8f2e7872ce
Jsmond2016 <jsmond2016@gmail.com>
2020-08-28 15:18:13 +0800

    一句话打开 Dialog，学习 render, h 的使用

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,24 +1,25 @@
 <template>
-  <h1>Dialog 示例</h1>
-  <div style="position: relative; z-index: 1;">
-    <Button @click="toggleDialog">切换Dialog</Button>
-    <!-- <Dialog :visible="x" @update:visible="x = $event" /> -->
-    <Dialog v-model:visible="x" :closeOnClickOverlay="true" :confirm="onOk" :cancel="onCancel">
-      <template v-slot:content>
-        <p>你好测试，你好测试</p>
-        <p>你好测试，你好测试</p>
-      </template>
-      <template v-slot:title>
-        <strong>我是标题</strong>
-      </template>
-    </Dialog>
-  </div>
-  <div style="position: relative; width: 500px; height: 500px; background: red; z-index: 2;">测试层叠关系</div>
+  <h1>Dialog 示例1</h1>
+  <Button @click="toggleDialog">切换Dialog</Button>
+  <!-- <Dialog :visible="x" @update:visible="x = $event" /> -->
+  <Dialog v-model:visible="x" :closeOnClickOverlay="true" :confirm="onOk" :cancel="onCancel">
+    <template v-slot:content>
+      <p>你好测试，你好测试</p>
+      <p>你好测试，你好测试</p>
+    </template>
+    <template v-slot:title>
+      <strong>我是标题</strong>
+    </template>
+  </Dialog>
+  <h1>Dialog 示例2</h1>
+  <Button @click="showDialog">一行代码打开 Dialog</Button>
 </template>
 <script>
 import Dialog from "../lib/Dialog.vue";
 import Button from "../lib/Button.vue";
 import { ref } from "vue";
+import { openDialog } from '../lib/openDialog';
+
 export default {
   components: {
     Dialog,
@@ -37,7 +38,16 @@ export default {
     const onCancel = () => {
       console.log("cancel");
     };
-    return { x, toggleDialog, onOk, onCancel };
+    const showDialog = () => {
+      openDialog({
+        title: "示例2-测试",
+        content: "你好",
+        closeOnClickOverlay: false,
+        ok: () => { console.log('okkkk-')},
+        cancel: () =>{ console.log('cancel-----')},
+      })
+    }
+    return { x, toggleDialog, onOk, onCancel, showDialog };
   },
 };
 </script>
