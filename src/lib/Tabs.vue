7bd81dac1031e3a73daa631d75cfb3578702e55d
Jsmond2016 <jsmond2016@gmail.com>
2020-09-09 18:17:36 +0800

    完成 Tabs 切换

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="my-tabs">
-    <div class="my-tabs-nav">
-      <div class="my-tabs-nav-item" :class="{selected: t === selected}" v-for="(t,index) in titles" :key="index">{{t}}</div>
+    <div class="my-tabs-nav" ref="container">
+      <div class="my-tabs-nav-item" :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :class="{selected: t === selected}" v-for="(t,index) in titles" :key="index">{{t}}</div>
+      <div class="my-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="my-tabs-content">
-      <component class="my-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+      <component :is="current" :key="current.props.title" />
     </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import Tab from './Tab.vue'
+import {
+  computed,
+  ref,
+  watchEffect,
+  onMounted
+} from 'vue'
 export default {
   components: {
     Tab,
@@ -21,17 +28,48 @@ export default {
     }
   },
   setup(props, context) {
+    const selectedItem = ref < HTMLDivElement > (null)
+    const indicator = ref < HTMLDivElement > (null)
+    const container = ref < HTMLDivElement > (null)
+    onMounted(() => {
+      watchEffect(() => {
+        const {
+          width
+        } = selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {
+          left: left1
+        } = container.value.getBoundingClientRect()
+        const {
+          left: left2
+        } = selectedItem.value.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left = left + 'px'
+      })
+    })
     const defaults = context.slots.default()
-    console.log(defaults[0].type === Tab);
-    defaults.forEach(tag => {
+    defaults.forEach((tag) => {
       if (tag.type !== Tab) {
-        throw new Error('Tabs 的子组件必须为 Tab')
+        throw new Error('Tabs 子标签必须是 Tab')
       }
     })
-    const titles = defaults.map(tag => tag.props.title)
+    const current = computed(() => {
+      return defaults.find(tag => tag.props.title === props.selected)
+    })
+    const titles = defaults.map((tag) => {
+      return tag.props.title
+    })
+    const select = (title: string) => {
+      context.emit('update:selected', title)
+    }
     return {
+      current,
       defaults,
-      titles
+      titles,
+      select,
+      selectedItem,
+      indicator,
+      container
     }
   }
 }
@@ -46,6 +84,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -56,6 +95,15 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
     }
   }
   &-content {
