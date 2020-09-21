c6b4dd1d3fac086c292699d5e3ec185a6350d3f8
Jsmond2016 <jsmond2016@gmail.com>
2020-09-21 18:23:11 +0800

    优化代码，消除重复

--- src/router.ts
+++ src/router.ts
@@ -6,9 +6,11 @@ import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
 import DocDemo from "./components/DocDemo.vue";
+import Markdown from './components/Markdown.vue';
 import Intro from "./views/Intro.vue";
 import GetStart from "./views/GetStart.vue";
 import Install from "./views/Install.vue";
+import { h } from 'vue';
 
 const history = createWebHashHistory();
 export const router = createRouter({
@@ -20,9 +22,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: "", component: DocDemo },
-        { path: "intro", component: Intro },
-        { path: "get-started", component: GetStart },
-        { path: "install", component: Install },
+        { path: "intro", component: h(Markdown, {path: '../markdown/intro.md', key: '1'}) },
+        { path: "get-started", component: h(Markdown, {path: '../markdown/get-started.md', key: '2'}) },
+        { path: "install", component: h(Markdown, {path: '../markdown/install.md', key: '3'}) },
         { path: "switch", component: SwitchDemo },
         { path: "button", component: ButtonDemo },
         { path: "dialog", component: DialogDemo },
