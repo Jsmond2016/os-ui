0d569591045ff200a2575fb1cd6534eee4877a43
Jsmond2016 <jsmond2016@gmail.com>
2020-09-21 14:58:31 +0800

    从 iconfont 引入图标

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="topnav">
-    <div class="logo">LOGO</div>
+    <div class="logo">
+      <svg class="icon">
+        <use xlink:href="#icon-snow"></use>
+      </svg>
+    </div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -25,6 +29,8 @@
   }
 </script>
 <style lang="scss" scoped>
+$color: rgb(22 144 138);
+
   .topnav {
     // background: pink;
     display: flex;
@@ -36,10 +42,14 @@
     z-index: 10;
     justify-content: center;
     align-items: center;
-
+    color: $color;
     >.logo {
       max-width: 6em;
       margin-right: auto;
+      > svg {
+        width: 32px;
+        height: 32px;
+      }
     }
 
     >.menu {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
   <div class="topnavAndBanner">
     <Topnav/>
     <div class="banner">
@@ -10,6 +11,18 @@
       </p>
     </div>
   </div>
+  <div class="features">
+    <svg class="icon">
+      <use xlink:href="#icon-vue"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="#icon-ts"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="#icon-light"></use>
+    </svg>
+  </div>
+</div>
 </template>
 <script lang="ts">
 import Topnav from '../components/Topnav.vue'
@@ -18,29 +31,41 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+$green: #02bcb0;
+$border-radius: 4px;
+$color: rgb(22 144 138);
+
 .topnavAndBanner {
   background: rgb(184,241,237);
   background: linear-gradient(90deg, rgba(184,241,237,1) 0%, rgba(125,216,235,1) 100%, rgba(183,233,230,1) 100%);
-} 
+}
 .banner {
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  color:$color;
   // background: lightgreen;
   > .actions{
     padding: 8px 0;
     a {
       margin:0 8px;
-      background: #fff;
+      background: $green;
+      color: white;
       display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+      &:hover{
+        text-decoration: none;
+      }
     }
+  }
+}
+.features {
+  > svg {
+    width: 64px;
+    height: 64px;
   }
 }
 </style>
